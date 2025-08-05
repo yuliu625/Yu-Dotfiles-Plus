15fab98ba1003871343d755343d25c6ac856fe30
yu <yul1024@outlook.com>
2025-08-05 10:52:21 +0800

    Create remove_files.py

---
+++ path_manager/remove_files.py
@@ -0,0 +1,36 @@
+"""
+危险方法！！！
+
+注意:
+    - 确保对于备份文件进行操作！一定要有备份！
+    - 这个方法仅用于传输至服务器时减轻压力。
+"""
+
+from __future__ import annotations
+
+from pathlib import Path
+
+from typing import TYPE_CHECKING
+# if TYPE_CHECKING:
+
+
+def remove_files(
+    root_dir: str | Path,
+    pattern: str,
+) -> None:
+    root_dir = Path(root_dir)
+    for file_path in root_dir.rglob(pattern):
+        file_path.unlink()
+        print(f"Deleted {file_path}")
+
+
+if __name__ == '__main__':
+    # 一定要检查是备份文件的路径！
+    root_dir_ = r""  # 注意！一定要检查！
+    pattern_ = r'*.db'
+
+    remove_files(
+        root_dir=root_dir_,
+        pattern=pattern_,
+    )
+
